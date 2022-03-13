6d3d285409eeea298e4e3acb0a08a982edef8a42
Murali Paluru <leodotcloud@gmail.com>
2022-03-12 23:18:56 -0800

    add apt update

--- box/customize.sh
+++ box/customize.sh
@@ -1,29 +1,32 @@
 #!/usr/bin/env bash
 
+set -e
+
 # Install additional packages
-sudo apt-get install -y \
-pip \
-unzip \
-jq \
-clang \
-build-essential \
-libhugetlbfs-dev \
-libsqlite3-dev \
-libssl-dev \
-lldb \
-net-tools \
-pkg-config \
-libcurl4-openssl-dev \
-libssl-dev \
-uuid-dev \
-zlib1g-dev \
-libpulse-dev \
-cmake \
-openjdk-11-jdk
+sudo apt-get update &&
+  apt-get install -y \
+    pip \
+    unzip \
+    jq \
+    clang \
+    build-essential \
+    libhugetlbfs-dev \
+    libsqlite3-dev \
+    libssl-dev \
+    lldb \
+    net-tools \
+    pkg-config \
+    libcurl4-openssl-dev \
+    libssl-dev \
+    uuid-dev \
+    zlib1g-dev \
+    libpulse-dev \
+    cmake \
+    openjdk-11-jdk
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
-wget https://services.gradle.org/distributions/gradle-7.3.1-bin.zip -P /tmp && \
-    unzip /tmp/gradle-7.3.1-bin.zip -d /usr/local/bin && \
-    ln -sv /usr/local/bin/gradle-7.3.1/bin/gradle /usr/bin && \
-    rm /tmp/gradle-7.3.1-bin.zip
+wget https://services.gradle.org/distributions/gradle-7.3.1-bin.zip -P /tmp &&
+  unzip /tmp/gradle-7.3.1-bin.zip -d /usr/local/bin &&
+  ln -sv /usr/local/bin/gradle-7.3.1/bin/gradle /usr/bin &&
+  rm /tmp/gradle-7.3.1-bin.zip
